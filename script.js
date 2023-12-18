2c2f6b5bf533ef2e1ae8e0960dfeefa012e53b36
Shreejal Maharjan <shreejal27@gmail.com>
2023-12-18 18:09:25 +0545

    added smoke on extinguished candle

--- script.js
+++ script.js
@@ -1,71 +1,99 @@
 let microphoneStream; // Variable to store the microphone stream
 function initMicrophone() {
-  // Check for Web Audio API support
-  if (!('AudioContext' in window || 'webkitAudioContext' in window)) {
-      alert('Web Audio API is not supported in this browser');
-      return;
-  }
 
-  // Create an audio context
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    turnOnFlame();
+    // Check for Web Audio API support
+    if (!('AudioContext' in window || 'webkitAudioContext' in window)) {
+        alert('Web Audio API is not supported in this browser');
+        return;
+    }
 
-  // Access the user's microphone
-  navigator.mediaDevices.getUserMedia({ audio: true })
-      .then((stream) => {
-          // Store the microphone stream in the variable
-          microphoneStream = stream;
+    // Create an audio context
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-          // Create an audio source node
-          const source = audioContext.createMediaStreamSource(stream);
+    // Access the user's microphone
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+            // Store the microphone stream in the variable
+            microphoneStream = stream;
 
-          // Create an analyser node
-          const analyser = audioContext.createAnalyser();
-          analyser.fftSize = 256; // Adjust the FFT size as needed
+            // Create an audio source node
+            const source = audioContext.createMediaStreamSource(stream);
 
-          // Connect the source to the analyser
-          source.connect(analyser);
+            // Create an analyser node
+            const analyser = audioContext.createAnalyser();
+            analyser.fftSize = 256; // Adjust the FFT size as needed
 
-          // Connect the analyser to the audio context's destination (speakers)
-          analyser.connect(audioContext.destination);
+            // Connect the source to the analyser
+            source.connect(analyser);
 
-          // Set up an array to store frequency data
-          const dataArray = new Uint8Array(analyser.frequencyBinCount);
+            // Connect the analyser to the audio context's destination (speakers)
+            analyser.connect(audioContext.destination);
 
-          // Check for blowing into the microphone periodically
-          setInterval(() => {
-              analyser.getByteFrequencyData(dataArray);
+            // Set up an array to store frequency data
+            const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-              // Analyze the frequency data (you may need to customize this part)
-              const maxFrequency = Math.max(...dataArray);
-              if (maxFrequency > 254) {
-                  console.log('User is blowing into the microphone!');
-                  document.getElementById('text').innerHTML = "Blowing";
-                  // Call the flameToggle function or place your logic here
-                  flameToggle();
-              }
-          }); // Adjust the interval as needed
-      })
-      .catch((error) => {
-          console.error('Error accessing microphone:', error);
-      });
+            // Check for blowing into the microphone periodically
+            setInterval(() => {
+                analyser.getByteFrequencyData(dataArray);
+
+                // Analyze the frequency data (you may need to customize this part)
+                const maxFrequency = Math.max(...dataArray);
+                if (maxFrequency > 254) {
+                    //   console.log('User is blowing into the microphone!');
+                    //   document.getElementById('text').innerHTML = "Blowing";
+                    // Call the flameToggle function or place your logic here
+                    turnOffFlame();
+                }
+            }); // Adjust the interval as needed
+        })
+        .catch((error) => {
+            console.error('Error accessing microphone:', error);
+        });
 }
 
-function flameToggle() {
-  var flame = document.getElementById('flame');
-  if (flame) {
-      flame.classList.remove('lit');
-      flame.classList.add('out');
+function turnOffFlame() {
+    var flame = document.getElementById('flame');
+    if (flame) {
+        flame.classList.remove('lit');
+        flame.classList.add('out');
 
-      // Stop the microphone input by stopping the audio track
-      if (microphoneStream) {
-          const audioTracks = microphoneStream.getAudioTracks();
-          if (audioTracks.length > 0) {
-              audioTracks[0].stop();
-          }
-      }
-  } else {
-      console.error('Flame element not found');
-  }
+        // Stop the microphone input by stopping the audio track
+        if (microphoneStream) {
+            const audioTracks = microphoneStream.getAudioTracks();
+            if (audioTracks.length > 0) {
+                audioTracks[0].stop();
+            }
+        }
+        addSmoke();
+    } else {
+        console.error('Flame element not found');
+    }
 }
 
-// flame.addEventListener('click', flameToggle);
+function turnOnFlame() {
+    var flame = document.getElementById('flame');
+    if (flame) {
+        flame.classList.remove('out');
+        flame.classList.add('lit');
+        removeSmoke();
+    } else {
+        console.error('Flame element not found');
+    }
+}
+
+// Function to add smoke divs
+function addSmoke() {
+    for (let i = 0; i < 3; i++) {
+        const smokeDiv = document.createElement("div");
+        smokeDiv.className = "smoke";
+        document.querySelector(".candle").appendChild(smokeDiv);
+    }
+}
+
+// Function to remove smoke divs
+function removeSmoke() {
+    const smokeDivs = document.querySelectorAll(".smoke");
+    smokeDivs.forEach(smokeDiv => smokeDiv.remove());
+}
+
